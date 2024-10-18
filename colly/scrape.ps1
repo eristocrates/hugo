25678f30a1fed94765872b5ce87d67e7a4a71e5d
eristocrates <reillocb@gmail.com>
2024-10-17 20:53:17 -0400

    Automated commit scraping data

---
+++ colly/scrape.ps1
@@ -0,0 +1,14 @@
+# Run the Go program and wait for it to finish
+Start-Process -FilePath "go" -ArgumentList "run ." -Wait
+
+# Navigate to your repository directory if not already there
+Set-Location ..\
+
+# Add changes to git
+git add .
+
+# Commit changes with a message
+git commit -m "Automated commit scraping data"
+
+# Push changes to the remote repository
+git push
